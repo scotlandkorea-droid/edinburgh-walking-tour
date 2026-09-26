64577eeed1ee6594700a718492491784e3d47afa
scotlandkorea-droid <scotlandkorea@gmail.com>
2026-09-26 14:25:01 +0100

    Serve directory index pages such as St Andrews

--- worker.js
+++ worker.js
@@ -1,10 +1,32 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const method = request.method.toUpperCase();
 
-    if (url.pathname === "/") {
-      const indexUrl = new URL("/index.html", url);
-      return env.ASSETS.fetch(new Request(indexUrl, request));
+    if (method === "GET" || method === "HEAD") {
+      if (url.pathname === "/") {
+        const indexUrl = new URL("/index.html", url);
+        return env.ASSETS.fetch(new Request(indexUrl, request));
+      }
+
+      if (url.pathname.endsWith("/")) {
+        const indexUrl = new URL(url);
+        indexUrl.pathname = url.pathname + "index.html";
+        const response = await env.ASSETS.fetch(new Request(indexUrl, request));
+        if (response.status !== 404) return response;
+      } else {
+        const lastSegment = url.pathname.split("/").pop() || "";
+        if (!lastSegment.includes(".")) {
+          const indexUrl = new URL(url);
+          indexUrl.pathname = url.pathname + "/index.html";
+          const response = await env.ASSETS.fetch(new Request(indexUrl, request));
+          if (response.status !== 404) {
+            const redirectUrl = new URL(url);
+            redirectUrl.pathname = url.pathname + "/";
+            return Response.redirect(redirectUrl.toString(), 308);
+          }
+        }
+      }
     }
 
     return env.ASSETS.fetch(request);
