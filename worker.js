829d854cb10fa2289d8d889b5648ae1ce23bef27
scotlandkorea-droid <scotlandkorea@gmail.com>
2026-09-26 14:47:43 +0100

    Handle St Andrews hub without redirect recursion

--- worker.js
+++ worker.js
@@ -9,23 +9,15 @@ export default {
         return env.ASSETS.fetch(new Request(indexUrl, request));
       }
 
-      if (url.pathname.endsWith("/")) {
-        const indexUrl = new URL(url);
-        indexUrl.pathname = url.pathname + "index.html";
-        const response = await env.ASSETS.fetch(new Request(indexUrl, request));
-        if (response.status !== 404) return response;
-      } else {
-        const lastSegment = url.pathname.split("/").pop() || "";
-        if (!lastSegment.includes(".")) {
-          const indexUrl = new URL(url);
-          indexUrl.pathname = url.pathname + "/index.html";
-          const response = await env.ASSETS.fetch(new Request(indexUrl, request));
-          if (response.status !== 404) {
-            const redirectUrl = new URL(url);
-            redirectUrl.pathname = url.pathname + "/";
-            return Response.redirect(redirectUrl.toString(), 308);
-          }
-        }
+      if (url.pathname === "/st-andrews") {
+        const redirectUrl = new URL(url);
+        redirectUrl.pathname = "/st-andrews/";
+        return Response.redirect(redirectUrl.toString(), 308);
+      }
+
+      if (url.pathname === "/st-andrews/") {
+        const indexUrl = new URL("/st-andrews/index.html", url);
+        return env.ASSETS.fetch(new Request(indexUrl, request));
       }
     }
 
